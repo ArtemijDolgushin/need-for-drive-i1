d2103c9e1c3a732544a9bb5d559041bc2c8f3a06
boo-mer <artemijdolgushin@gmail.com>
2021-11-02 11:44:33 +0300

    F-03 Рефакторинг обращения к API

--- src/API.js
+++ src/API.js
@@ -1,7 +1,10 @@
+const token = process.env.VUE_APP_API_TOKEN;
+const url = process.env.VUE_APP_API_URL;
+
 export class API {
     static async get(endpoint) {
-        const request = await fetch(`${endpoint}`, {
-            headers: {'X-Api-Factory-Application-Id': process.env.VUE_APP_API_TOKEN}
+        const request = await fetch(`${url}${endpoint}`, {
+            headers: {'X-Api-Factory-Application-Id': token}
         });
         return await request.json();
     }
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
 process.env.VUE_APP_API_TOKEN = '5e25c641099b810b946c5d5b';
+process.env.VUE_APP_API_URL = 'https://api-factory.simbirsoft1.com/api/db';
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/need-for-drive-i1/'
         : '/',
-    devServer: {
-        proxy: "https://api-factory.simbirsoft1.com/api/db",
-    },
     css: {
         loaderOptions: {
             sass: {
