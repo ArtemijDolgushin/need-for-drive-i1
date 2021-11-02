7243610cdcfbfc086d0ebdee7d92c9ed409a0640
boo-mer <artemijdolgushin@gmail.com>
2021-11-02 11:28:09 +0300

    F-03 Рефакторинг обращения к API

--- src/API.js
+++ src/API.js
@@ -1,14 +1,16 @@
 export class API {
-    static async getCities() {
-        const request = await fetch("https://api-factory.simbirsoft1.com/api/db/city", {
-            headers: {'X-Api-Factory-Application-Id': "5e25c641099b810b946c5d5b"}
+    static async get(endpoint) {
+        const request = await fetch(`${endpoint}`, {
+            headers: {'X-Api-Factory-Application-Id': process.env.VUE_APP_API_TOKEN}
         });
         return await request.json();
     }
+
+    static async getCities() {
+        return await this.get('/city');
+    }
+
     static async getPoints() {
-        const request = await fetch("https://api-factory.simbirsoft1.com/api/db/point", {
-            headers: {'X-Api-Factory-Application-Id': "5e25c641099b810b946c5d5b"}
-        });
-        return await request.json();
+        return await this.get('/point');
     }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
+process.env.VUE_APP_API_TOKEN = '5e25c641099b810b946c5d5b';
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/need-for-drive-i1/'
         : '/',
+    devServer: {
+        proxy: "https://api-factory.simbirsoft1.com/api/db",
+    },
     css: {
         loaderOptions: {
             sass: {
